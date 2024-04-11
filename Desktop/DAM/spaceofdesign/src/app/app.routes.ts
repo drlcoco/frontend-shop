61b2cfa02790aee607021f8156857457ba32af51
David De los reyes luque <davidrl@MacBook-Pro-de-David.local>
2024-04-11 11:51:47 +0200

    Entrega primera versión 1.0.0 terminada

--- Desktop/DAM/spaceofdesign/src/app/app.routes.ts
+++ Desktop/DAM/spaceofdesign/src/app/app.routes.ts
@@ -1,54 +1,31 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
-import { HowWeDoItComponent } from './component/how-we-do-it/how-we-do-it.component';
-import { ContactComponent } from './component/contact/contact.component';
 import { LanguageComponent } from './component/language/language.component';
-import { SlidingComponent } from './component/sliding/sliding.component';
-import { RollerComponent } from './component/roller/roller.component';
-import { PleatedComponent } from './component/pleated/pleated.component';
-import { SlatedComponent } from './component/slated/slated.component';
-import { VenetianComponent } from './component/venetian/venetian.component';
-import { TextileComponent } from './component/textile/textile.component';
-import { AwningSystemComponent } from './component/awning-system/awning-system.component';
-import { CustomStructuresComponent } from './component/custom-structures/custom-structures.component';
-import { PergolasComponent } from './component/pergolas/pergolas.component';
-import { BioclimaticComponent } from './component/bioclimatic/bioclimatic.component';
-import { LedComponent } from './component/led/led.component';
-import { CoffeeComponent } from './component/coffee/coffee.component';
-import { RetractableComponent } from './component/retractable/retractable.component';
-import { FlatComponent } from './component/flat/flat.component';
-import { OutdoorVerticalComponent } from './component/outdoor-vertical/outdoor-vertical.component';
-import { TraditionalComponent } from './component/traditional/traditional.component';
-import { IndoorComponent } from './component/indoor/indoor.component';
-import { OutdoorComponent } from './component/outdoor/outdoor.component';
-import { ParasolsComponent } from './component/parasols/parasols.component';
-import { RotatingComponent } from './component/rotating/rotating.component';
-import { CrystalComponent } from './component/crystal/crystal.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'work', component: HowWeDoItComponent },
-  { path: 'contact', component: ContactComponent },
   { path: 'language', component: LanguageComponent },
-  { path: 'sliding', component: SlidingComponent },
-  { path: 'roller', component: RollerComponent },
-  { path: 'pleated', component: PleatedComponent },
-  { path: 'slated', component: SlatedComponent },
-  { path: 'venetian', component: VenetianComponent },
-  { path: 'textile', component: TextileComponent },
-  { path: 'awning', component: AwningSystemComponent },
-  { path: 'customstructures', component: CustomStructuresComponent },
-  { path: 'pergolas', component: PergolasComponent },
-  { path: 'bioclimatic', component: BioclimaticComponent },
-  { path: 'led', component: LedComponent },
-  { path: 'chest', component: CoffeeComponent },
-  { path: 'retractil', component: RetractableComponent },
-  { path: 'flat', component: FlatComponent },
-  { path: 'vertical', component: OutdoorVerticalComponent },
-  { path: 'traditional', component: TraditionalComponent },
-  { path: 'indoor', component: IndoorComponent },
-  { path: 'outdoor', component: OutdoorComponent },
-  { path: 'parasols', component: ParasolsComponent },
-  { path: 'rotating', component: RotatingComponent },
-  { path: 'crystal', component: CrystalComponent }
+  { path: 'work', loadComponent: () => import('./component/how-we-do-it/how-we-do-it.component').then(c => c.HowWeDoItComponent)},
+  { path: 'contact', loadComponent: () => import('./component/contact/contact.component').then(c => c.ContactComponent)},
+  { path: 'sliding', loadComponent: () => import('./component/sliding/sliding.component').then(c => c.SlidingComponent)},
+  { path: 'roller', loadComponent: () => import('./component/roller/roller.component').then(c => c.RollerComponent)},
+  { path: 'pleated', loadComponent: () => import('./component/pleated/pleated.component').then(c => c.PleatedComponent)},
+  { path: 'slated', loadComponent: () => import('./component/slated/slated.component').then(c => c.SlatedComponent)},
+  { path: 'venetian', loadComponent: () => import('./component/venetian/venetian.component').then(c => c.VenetianComponent)},
+  { path: 'textile', loadComponent: () => import('./component/textile/textile.component').then(c => c.TextileComponent)},
+  { path: 'awning', loadComponent: () => import('./component/awning-system/awning-system.component').then(c => c.AwningSystemComponent)},
+  { path: 'customstructures', loadComponent: () => import('./component/custom-structures/custom-structures.component').then(c => c.CustomStructuresComponent)},
+  { path: 'pergolas', loadComponent: () => import('./component/pergolas/pergolas.component').then(c => c.PergolasComponent)},
+  { path: 'bioclimatic', loadComponent: () => import('./component/bioclimatic/bioclimatic.component').then(c => c.BioclimaticComponent)},
+  { path: 'led', loadComponent: () => import('./component/led/led.component').then(c => c.LedComponent)},
+  { path: 'chest', loadComponent: () => import('./component/coffee/coffee.component').then(c => c.CoffeeComponent)},
+  { path: 'retractil', loadComponent: () => import('./component/retractable/retractable.component').then(c => c.RetractableComponent)},
+  { path: 'flat', loadComponent: () => import('./component/flat/flat.component').then(c => c.FlatComponent)},
+  { path: 'vertical', loadComponent: () => import('./component/outdoor-vertical/outdoor-vertical.component').then(c => c.OutdoorVerticalComponent)},
+  { path: 'traditional', loadComponent: () => import('./component/traditional/traditional.component').then(c => c.TraditionalComponent)},
+  { path: 'indoor', loadComponent: () => import('./component/indoor/indoor.component').then(c => c.IndoorComponent)},
+  { path: 'outdoor', loadComponent: () => import('./component/outdoor/outdoor.component').then(c => c.OutdoorComponent)},
+  { path: 'parasols', loadComponent: () => import('./component/parasols/parasols.component').then(c => c.ParasolsComponent)},
+  { path: 'rotating', loadComponent: () => import('./component/rotating/rotating.component').then(c => c.RotatingComponent)},
+  { path: 'crystal', loadComponent: () => import('./component/crystal/crystal.component').then(c => c.CrystalComponent)}
 ];
